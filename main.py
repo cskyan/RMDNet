6afbf215827335751050c7683705d73aa62c28a0
zhang1133546 <72983443+zhang1133546@users.noreply.github.com>
2024-09-12 21:44:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 
-sys.path.append('/home/jiangbo/PhD/DBODL/DBODL.py')
+sys.path.append('/home/.../DBODL.py')
 
 class RunCmd(object):
   def cmd_run(self, cmd):
@@ -12,14 +12,14 @@ class RunCmd(object):
 for i in range(1):
     a = RunCmd()
     a.cmd_run('CUDA_VISIBLE_DEVICES=0 python DBODL.py \
-    --posi=/home/jiangbo/PhD/DBODL/GraphProt_CLIP_sequences/ALKBH5_Baltz2012.train.positives.fa \
-    --nega=/home/jiangbo/PhD/DBODL/GraphProt_CLIP_sequences/ALKBH5_Baltz2012.train.negatives.fa \
+    --posi=/home/.../DBODL/GraphProt_CLIP_sequences/ALKBH5_Baltz2012.train.positives.fa \
+    --nega=/home/.../GraphProt_CLIP_sequences/ALKBH5_Baltz2012.train.negatives.fa \
     --train=True --n_epochs=1 ')
 
     for j in range(1):
         a = RunCmd()
         a.cmd_run('CUDA_VISIBLE_DEVICES=0 python DBODL.py \
-        --testfile=/home/jiangbo/PhD/DBODL/GraphProt_CLIP_sequences/ALKBH5_Baltz2012.ls.positives.fa \
-        --nega=/home/jiangbo/PhD/DBODL/GraphProt_CLIP_sequences/ALKBH5_Baltz2012.ls.negatives.fa \
+        --testfile=/home/.../DBODL/GraphProt_CLIP_sequences/ALKBH5_Baltz2012.ls.positives.fa \
+        --nega=/home/.../DBODL/GraphProt_CLIP_sequences/ALKBH5_Baltz2012.ls.negatives.fa \
         --predict=True ')
 
